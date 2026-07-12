0265981ee32b7b96ba61310bd2c800aa4a3049dc
Abhay Gupta <abhaygupta8299@gmail.com>
2026-07-12 13:27:39 +0530

    Created portfolio HTML structure

--- hello.php
+++ hello.php
@@ -1,4 +1,6 @@
 <?php
 echo "<h1>Hello, PHP is Working!</h1>";
 echo "<p>Welcome Abhay!</p>";
+echo "<p>Welcome to the world of PHP programming.</p>";
+echo "<p>PHP is a server-side scripting language designed for web development but also used as a general-purpose programming language.</p>";
 ?>

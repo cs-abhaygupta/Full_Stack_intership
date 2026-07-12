5091f962e8b6293415a361848e28da4280ec97a7
Abhay Gupta <abhaygupta8299@gmail.com>
2026-07-13 00:28:34 +0530

    added javascript intractivity and made portfolio responsive

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload =function(){
+alert("Hello, welcome to Abhay's portfolio!");
+};
+const newLocal = document.getElementById("helloButton");
+const button = newLocal;
+button.addEventListener("click",() => {
+        alert("Hello, welcome");
+    });
+
+  document.getElementById("contact").addEventListener("submit",function (event){
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const message = document.getElementById("message").value;
+            if(name === "" || email === "" || message === ""){
+                alert("Please fill in all fields before submitting the form.");
+                event.preventDefault();
+            }            
+        });
+        
